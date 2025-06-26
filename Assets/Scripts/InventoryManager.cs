3674296c9695b68396eeeacd2dfe546ab1d84e55
Edward <edwardtichaud@gmail.com>
2025-06-26 19:43:53 +0200

    SquadManager

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -6,14 +6,6 @@ public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance { get; private set; }
 
-        if (GameManager.Instance != null && GameManager.Instance.gameData != null)
-        {
-            allItems = GameManager.Instance.gameData.allItems;
-            inventoryItems = GameManager.Instance.gameData.inventoryItems;
-        }
-
-            GameManager.Instance.gameData.inventoryItems.Add(item);
-        GameManager.Instance.gameData.inventoryItems.Remove(item);
     [Header("Tous les items du jeu (à assigner dans l’inspecteur ou à charger dynamiquement)")]
     [SerializeField] private List<ItemData> allItems = new();
 
