6e0598307f35efdce514cb522b3c9b27b8256899
Edward <edwardtichaud@gmail.com>
2025-07-12 09:53:17 +0200

    update

--- Assets/StartManager.cs
+++ Assets/StartManager.cs
@@ -17,4 +17,12 @@ public class StartManager : MonoBehaviour
         Debug.Log("Resetting Time Scale to 1");
         Time.timeScale = 1f;
     }
+
+    void ResetCamerasDepth()
+    {
+        Debug.Log("Resetting Camera Depths");
+        Camera.main.depth = 1;
+        Camera battleCamera = GameObject.FindGameObjectWithTag("BattleCamera").GetComponent<Camera>();
+        battleCamera.depth = 0;
+    }
 }
