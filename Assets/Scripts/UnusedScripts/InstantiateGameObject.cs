6f592b3d0b24a34d2d87a84bd87cf871782ed6ff
Edward <edwardtichaud@gmail.com>
2025-08-03 21:33:21 +0200

    u

--- Assets/Scripts/UnusedScripts/InstantiateGameObject.cs
+++ Assets/Scripts/UnusedScripts/InstantiateGameObject.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class InstantiateGameObject : MonoBehaviour
 {
+    public bool instantiateAtStart = false;
     public GameObject gameObjectToInstantiate;
 
     void Start()
     {
-        Instantiate(gameObjectToInstantiate, transform.position, Quaternion.identity);
+        if (instantiateAtStart)
+        {
+            Instantiate(gameObjectToInstantiate, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void InstantiateFromTimeline(GameObject gameObjectToInstantiate)
+    {
+        Instantiate(gameObjectToInstantiate, NewBattleManager.Instance.currentCharacterUnit.transform.position, Quaternion.identity);
     }
 }
