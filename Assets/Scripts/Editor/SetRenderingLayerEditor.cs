3fb2513736bd7b1ef01ee8b5e0e78ae6d007f349
EdwardTichaud <edwardtichaud@gmail.com>
2025-08-08 16:40:10 +0200

    Corrige l'erreur de compilation liée à GUILayout

--- Assets/Scripts/Editor/SetRenderingLayerEditor.cs
+++ Assets/Scripts/Editor/SetRenderingLayerEditor.cs
@@ -1,5 +1,6 @@
 #if UNITY_EDITOR
 using UnityEditor;
+using UnityEngine; // Nécessaire pour l'utilisation de GUILayout dans l'inspecteur
 
 /// <summary>
 /// Inspecteur personnalisé pour <see cref="SetRenderingLayer"/>.
