5b5af39a477300f58909e616bf60c99f75445c7c
EdwardTichaud <edwardtichaud@gmail.com>
2025-09-14 13:37:03 +0200

    Remplace l'usage de CinemachineCamera par le transform

--- Assets/Scripts/MonoBehavioursUsed/FollowBattleCaster.cs
+++ Assets/Scripts/MonoBehavioursUsed/FollowBattleCaster.cs
@@ -1,25 +1,18 @@
 using UnityEngine;
-using Unity.Cinemachine;
 
-/// Suit le lanceur actuel (caster) avec un décalage (Body = CinemachineFollow).
-[RequireComponent(typeof(CinemachineCamera))]
+/// Suit le lanceur actuel (caster) avec un décalage.
 public class FollowBattleCaster : MonoBehaviour
 {
     [Tooltip("Décalage appliqué par rapport à la position du lanceur.")]
     public Vector3 offset = new(0, 2, -3);
 
-    private CinemachineCamera cmCamera;
-
-    void Awake() => cmCamera = GetComponent<CinemachineCamera>();
-
     void LateUpdate()
     {
+        // Récupère le lanceur actuellement actif dans le combat.
         var caster = NewBattleManager.Instance?.currentCharacterUnit;
         if (!caster) return;
 
-        cmCamera.Follow = caster.transform;
-
-        if (cmCamera.GetCinemachineComponent(CinemachineCore.Stage.Body) is CinemachineFollow follow)
-            follow.FollowOffset = offset;
+        // Place la caméra directement par rapport au lanceur, en tenant compte de l'offset.
+        transform.position = caster.transform.position + offset;
     }
 }
